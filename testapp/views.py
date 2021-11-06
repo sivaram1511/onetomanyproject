cbbb17448b8faf204f97ae62093dcff621e8584e
sivaram1511 <sivaramk1411@gmail.com>
2021-11-06 12:30:24 +0530

    create author and book viewclasses

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
+from testapp.models import Author,Book
+from testapp.serializers import AuthorSerializer,BookSerializer
+from rest_framework import generics
+class AuthorListView(generics.ListCreateAPIView):
+    queryset=Author.objects.all()
+    serializer_class=AuthorSerializer
+class AuthorView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Author.objects.all()
+    serializer_class=AuthorSerializer
+class BookListView(generics.ListCreateAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
+class BookView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
