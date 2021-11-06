45e2d3fe75356360062c7c94de9e724ac120501d
sivaram1511 <sivaramk1411@gmail.com>
2021-11-06 12:31:14 +0530

    book and author serializer claases

--- testapp/serializers.py
+++ testapp/serializers.py
@@ -1,1 +1,10 @@
 from testapp.models import Author,Book
+from rest_framework import serializers
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Author
+        fields="__all__"
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Book
+        fields="__all__"
