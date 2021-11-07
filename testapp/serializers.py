99b3cc0e9fb97393b6a4aefbbe73ecc025c493f9
sivaram1511 <sivaramk1411@gmail.com>
2021-11-07 12:47:19 +0530

    write nested serializer in side author

--- testapp/serializers.py
+++ testapp/serializers.py
@@ -1,10 +1,11 @@
 from testapp.models import Author,Book
 from rest_framework import serializers
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Author
-        fields="__all__"
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model=Book
+        fields="__all__"
+class AuthorSerializer(serializers.ModelSerializer):
+    books_by_author=BookSerializer(read_only=True,many=True)
+    class Meta:
+        model=Author
         fields="__all__"
