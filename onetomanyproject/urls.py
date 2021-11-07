f38403299d8c127caa3319c47b60e481eeab91d2
sivaram1511 <sivaramk1411@gmail.com>
2021-11-07 12:20:40 +0530

    configure bookviews class url path

--- onetomanyproject/urls.py
+++ onetomanyproject/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("author-api",views.AuthorListView.as_view()),
     path("author-api/<pk>",views.AuthorView.as_view()),
+    path("book-api",views.BookListView.as_view()),
+    path('book-api/<pk>',views.BookView.as_view()),
+
 ]
